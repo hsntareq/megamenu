d6b4eaff20c6970465d3bc4ac0d030d3f1304935
Hasan Tareq <hsntareq@gmail.com>
2022-04-05 14:16:56 +0600

    init

---
+++ src/init.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Blocks Initializer
+ *
+ * Enqueue CSS/JS of all the blocks.
+ *
+ * @since   1.0.0
+ * @package CGB
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Enqueue Gutenberg block assets for both frontend + backend.
+ *
+ * Assets enqueued:
+ * 1. blocks.style.build.css - Frontend + Backend.
+ * 2. blocks.build.js - Backend.
+ * 3. blocks.editor.build.css - Backend.
+ *
+ * @uses {wp-blocks} for block type registration & related functions.
+ * @uses {wp-element} for WP Element abstraction — structure of blocks.
+ * @uses {wp-i18n} to internationalize the block's text.
+ * @uses {wp-editor} for WP editor styles.
+ * @since 1.0.0
+ */
+function megamenu_cgb_block_assets() { // phpcs:ignore
+	// Register block styles for both frontend + backend.
+	wp_register_style(
+		'megamenu-cgb-style-css', // Handle.
+		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
+		is_admin() ? array( 'wp-editor' ) : null, // Dependency to include the CSS after it.
+		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: File modification time.
+	);
+
+	// Register block editor script for backend.
+	wp_register_script(
+		'megamenu-cgb-block-js', // Handle.
+		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ), // Dependencies, defined above.
+		null, // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
+		true // Enqueue the script in the footer.
+	);
+
+	// Register block editor styles for backend.
+	wp_register_style(
+		'megamenu-cgb-block-editor-css', // Handle.
+		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
+		array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
+		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
+	);
+
+	// WP Localized globals. Use dynamic PHP stuff in JavaScript via `cgbGlobal` object.
+	wp_localize_script(
+		'megamenu-cgb-block-js',
+		'cgbGlobal', // Array containing dynamic data for a JS Global.
+		array(
+			'pluginDirPath' => plugin_dir_path( __DIR__ ),
+			'pluginDirUrl'  => plugin_dir_url( __DIR__ ),
+			// Add more data here that you want to access from `cgbGlobal` object.
+		)
+	);
+
+	/**
+	 * Register Gutenberg block on server-side.
+	 *
+	 * Register the block on server-side to ensure that the block
+	 * scripts and styles for both frontend and backend are
+	 * enqueued when the editor loads.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/blocks/writing-your-first-block-type#enqueuing-block-scripts
+	 * @since 1.16.0
+	 */
+	register_block_type(
+		'cgb/block-megamenu',
+		array(
+			// Enqueue blocks.style.build.css on both frontend & backend.
+			'style'         => 'megamenu-cgb-style-css',
+			// Enqueue blocks.build.js in the editor only.
+			'editor_script' => 'megamenu-cgb-block-js',
+			// Enqueue blocks.editor.build.css in the editor only.
+			'editor_style'  => 'megamenu-cgb-block-editor-css',
+		)
+	);
+}
+
+// Hook: Block assets.
+add_action( 'init', 'megamenu_cgb_block_assets' );
+
+
+
+
+// create new endpoint route
+add_action(
+	'rest_api_init',
+	function () {
+		register_rest_route(
+			'wp/v2',
+			'menu',
+			array(
+				'methods'             => 'GET',
+				'callback'            => 'custom_wp_menu',
+				'permission_callback' => '__return_true',
+			),
+		);
+	}
+);
+
+// create custom function to return nav menu
+function custom_wp_menu() {
+	return wp_get_nav_menu_items('MegaMenu');
+}
+
